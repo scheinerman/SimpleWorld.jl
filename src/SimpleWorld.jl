16e6c27cdbbe2e194dbe33df062a2c0461d7cbaf
Ed Scheinerman <ers@jhu.edu>
2016-05-07 15:41:27 -0400

    First version

---
+++ src/SimpleWorld.jl
@@ -0,0 +1,48 @@
+"""
+`SimplePackageLoader(pkg_name,verbose=false)`:
+This is a function form of the `using` command in Julia.
+Instead of `using SomePackage` we can instead call
+`SimplePackageLoader("SomePackage")`. With `verbose` set to
+`true` each `using` command is printed.
+
+This can also be called with a list of package names.
+"""
+function SimplePackageLoader(pkg_name::ASCIIString,
+        verbose::Bool=false)
+  command = "using $pkg_name"
+  if verbose
+    println(command)
+  end
+  eval(parse(command))
+  nothing
+end
+
+function SimplePackageLoader(pkgs::Array{ASCIIString,1},
+  verbose::Bool=false)
+  for p in pkgs
+    SimplePackageLoader(p,verbose)
+  end
+end
+
+"""
+`SimpleFavorites()` will load my favorite `Simple`
+graph theory packages, namely:
+
+* `SimpleGraphs`
+* `SimpleGraphAlgorithms`
+* `SimpleGraphDrawings`
+* `SimpleGraphRepresentations`
+
+as well as `ShowSet` and `PyPlot`.
+"""
+function SimpleFavorites(verbose::Bool=false)
+  packages = [
+  "SimpleGraphs"
+  "SimpleGraphRepresentations"
+  "SimpleGraphDrawings"
+  "SimpleGraphAlgorithms"
+  "ShowSet"
+  "PyPlot"
+  ]
+  SimplePackageLoader(packages,verbose)
+end
