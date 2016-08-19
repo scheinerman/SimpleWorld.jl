e5c95220667189242f7c08136f9b34112f99d443
Ed Scheinerman <ers@jhu.edu>
2016-08-19 15:58:45 -0400

    Fancier boxing

--- src/.juliarc.jl
+++ src/.juliarc.jl
@@ -3,6 +3,13 @@
 # Julia starts up.
 
 include(".julia/v0.4/SimpleWorld/src/SimpleWorld.jl")
-println("+"*"-"^47*"+")
-println("| Load favorite packages with SimpleFavorites() |")
-println("+"*"-"^47*"+")
+
+println("$(Char(9484))" * "$(Char(9472))"^47 * "$(Char(9488))")
+println("$(Char(9474))" * " Load favorite packages with SimpleFavorites() " *
+  "$(Char(9474))")
+println("$(Char(9492))" * "$(Char(9472))"^47 * "$(Char(9496))")
+
+#
+# println("+"*"-"^47*"+")
+# println("| Load favorite packages with SimpleFavorites() |")
+# println("+"*"-"^47*"+")
