83766673f3d9949e7148a0d0ccc79abc0818fd80
Ed Scheinerman <ers@jhu.edu>
2018-09-22 14:05:59 -0400

    Unregistered packages to into dev

--- src/Rebuild-All-Packages.jl
+++ src/Rebuild-All-Packages.jl
@@ -32,10 +32,10 @@ my_unregistered=[
     "SimplePosetAlgorithms",
     "SimplePosetDrawings",
     "SimplePosetRepresentations",
-    "SimplePosets"
+    "SimplePosets",
+    "SimpleWorld"
 ]
 
-my_dev = ["SimpleWorld"]
 
 
 julia_packages=[
@@ -65,7 +65,7 @@ julia_packages=[
 
 function load_my_registered()
   for pkg in my_registered
-    @info "Adding: $pkg"
+    @info "Adding scheinerman registered: $pkg"
     Pkg.add(pkg)
   end
   nothing
@@ -73,7 +73,7 @@ end
 
 function load_julia_favorites()
   for pkg in julia_packages
-    @info "Adding: $pkg"
+    @info "Adding Julia package: $pkg"
     Pkg.add(pkg)
   end
   nothing
@@ -87,37 +87,17 @@ function load_unreg(pkg_name::String)
   pre = "https://github.com/scheinerman/"
   post = ".jl.git"
   full_name = pre*pkg_name*post
-  Pkg.add(PackageSpec(url=full_name))
-  nothing
-end
-
-function load_dev(pkg_name::String)
-  pre = "https://github.com/scheinerman/"
-  post = ".jl.git"
-  full_name = pre*pkg_name*post
   Pkg.develop(PackageSpec(url=full_name))
   nothing
 end
 
-function load_my_dev()
-    for pkg in my_dev
-        @info "Loading $pkg for development"
-        try
-            load_dev(pkg)
-        catch
-            @info "Unable to load $pkg"
-        end
-    end
-    nothing
-end
-
 function load_my_unregistered()
   for pkg in my_unregistered
-    @info "Loading $pkg"
+    @info "Loading $pkg into development"
     try
       load_unreg(pkg)
     catch
-      @info ("Unable to load $pkg")
+      @warn "FAILED to load $pkg"
     end
   end
   nothing
--- src/SimpleWorld.jl
+++ src/SimpleWorld.jl
@@ -1,9 +1,7 @@
-# This file serves no useful purpose.
 
 module SimpleWorld
 
-export load_my_registered, load_julia_favorites, load_unreg, load_dev
-export load_my_dev, load_my_unregistered, load_all
+export load_my_registered, load_julia_favorites, load_unreg, load_all
 
 include("Rebuild-All-Packages.jl")
 
